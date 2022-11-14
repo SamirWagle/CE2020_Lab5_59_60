12ba62c45b14ed29063f20e884942bc694d20662
Samir Wagle <waglesameer5@gmail.com>
2022-11-14 14:49:00 +0545

    main.cpp code

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include"sort.h"
+#include"sort.cpp"
+#include<vector>
+#include<time.h>
+
+
+using namespace std;
+int main()
+{
+    vector<int> array;
+    int n=50;
+srand(time(nullptr));
+for(int i=0;i<n;i++){
+array.push_back(rand()%200);
+}
+cout<<"Using Rand"<<endl;
+for(int i=0;i<n;i++){
+    cout<<array.at(i)<<", ";
+}
+
+}
