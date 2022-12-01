afd9deead28bdce81fcd7877becfd9f87471f255
Samir Wagle <waglesameer5@gmail.com>
2022-12-01 23:22:24 +0545

    finalized code

--- main.cpp
+++ main.cpp
@@ -1,36 +1,80 @@
-#include<iostream>
-#include"sort.h"
-#include"sort.cpp"
-#include<vector>
-#include<time.h>
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <random>
+#include <chrono>
+#include "sort.cpp"
+#include "sort.h"
 
-
+using namespace std::chrono;
 using namespace std;
-
 
 int main()
 {
-    vector<int> array;
-    int n=50000;
-srand(time(nullptr));
-for(int i=0;i<n;i++){
-array.push_back(rand()%200);
-}
+    int size = 0, times;
+    string choice;
+    bool repeat = true;
+
+    cout << "\nHow many times do you want to sort: ";
+    cin >> times;
+
+    srand(time(nullptr));
+
+    for (int i = 1; i <= times; i++)
+    {
+        std::vector<int> vector1;
+        std::vector<int> vector2;
+
+        cout << "\nHow many numbers do you want to sort:";
+        cin >> size;
+           for (int j = 0; j < size; j++)
+        {
+            int value = rand() % 500;
+            vector1.push_back(value);
+            vector2.push_back(value);
+        }
+
+     
+        cout << "\nRandom values generated and stored successfully" << endl;
+
+     cout<<"For Quick Sort"<<endl;
+
+        auto startquick = high_resolution_clock::now();
+        QuickSort(vector1,0,size);
+        auto stopquick = high_resolution_clock::now();
+        auto durationquick = duration_cast<milliseconds>(stopquick - startquick);
+
+        cout << "\n******************************" << endl;
+
+        cout << "\nTOTAL TIME (MILLISECOND) REQUIRED TO SORT THE DATA USING QUICKSORT:" << durationquick.count() << endl;
+
+        cout << "\n******************************\n\n\n\n" << endl;
 
 
-cout<<"Using Rand"<<endl;
-for(int i=0;i<n;i++){
-    cout<<array.at(i)<<", ";
-}
-cout<<"/n/nTime is:"<<endl;
-cout<<time(nullptr)<<endl;
-InsertionSort(array,n);
-cout<<"/n/nTime is:"<<endl;
-cout<<time(nullptr)<<endl;
-cout<<"Quick Sort"<<endl;
-for(int i=0;i<n;i++){
-    cout<<array.at(i)<<", ";
-}
 
 
+
+            cout<<"For Insertion Sort"<<endl;
+
+        auto startquick = high_resolution_clock::now();
+       InsertionSort(vector2,size);
+        auto stopquick = high_resolution_clock::now();
+        auto durationquick = duration_cast<milliseconds>(stopquick - startquick);
+
+        cout << "\n******************************" << endl;
+
+        cout << "\nTOTAL TIME (MILLISECOND) REQUIRED TO SORT THE DATA USING INSERTIONSORT:" << durationquick.count() << endl;
+
+        cout << "\n******************************\n\n\n\n" << endl;
+    
+
+
+
+
+
+
+
+
+
+    }
 }
--- sort.cpp
+++ sort.cpp
@@ -21,41 +21,37 @@ void InsertionSort(vector <int> arr,int size){
 
     }
     cout<<"\nInsertion Sort"<<endl;
-    for(int i=0;i<size;i++){
-    cout<<arr[i]<<", ";
-}
+
     return;
 };
 
-void Quicksort(vector<int>arr,int size,int left,int right){
-    int i=left;
-    int j=right;
-    int pivot=arr[(left+right)/2];
-    while(i<=j){
-        while(arr[i]<pivot){
-            i++;
-        }
-        while(arr[j]>pivot){
-            j--;
-        }
-        if(i<=j){
-            arr[i]=arr[i]^arr[j];
-            arr[j]=arr[i]^arr[j];
-            arr[i]=arr[i]^arr[j];
-            i++;
-            j--;
+int partition(std::vector<int> & vect, int small, int large)
+{
+    int pivot = vect[large];
+    int i = small-1;
+    for(int j=small;j<large;j++){
+        if(vect[j]<=pivot){
+            i=i+1;
+            int temp1=vect[i];
+            vect[i]=vect[j];
+            vect[j]=temp1;   
         }
     }
-    if(left<j){
-        Quicksort(arr,size,left,j);
-    }
-    if(i<right){
-        Quicksort(arr,size,i,right);
-    }
-
-    cout<<"\n Sort"<<endl;
-    for(int i=0;i<size;i++){
-    cout<<arr[i]<<", ";
+    
+    int temp2 = vect[i+1];
+    vect[i+1] = vect[large];
+    vect[large] = temp2;
+    return i+1;
 }
-    return;
-};
+
+void QuickSort(std::vector<int> & vect, int low, int high)
+{
+    int pivot;
+
+    if (low < high)
+    {
+        pivot = partition(vect, low, high);
+        QuickSort(vect, low, pivot - 1);
+        QuickSort(vect, pivot + 1, high);
+    }
+}
